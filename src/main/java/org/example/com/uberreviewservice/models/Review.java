528607df4791ca4096f21003cc165ff6396c9ade
CodeWithLychee <abhi.sharma10220@gmail.com>
2025-11-13 19:35:20 +0530

    one model done

--- src/main/java/org/example/com/uberreviewservice/models/Review.java
+++ src/main/java/org/example/com/uberreviewservice/models/Review.java
@@ -1,12 +1,33 @@
 package org.example.com.uberreviewservice.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "Booking_Review")
 public class Review {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //identity means auto increment
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate // only at object creation
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // when object update
+    Date updatedAt;
 }
