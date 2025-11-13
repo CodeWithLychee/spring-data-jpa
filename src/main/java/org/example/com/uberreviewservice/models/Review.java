6788ff794625a896cb924d5f6bafdead81965948
CodeWithLychee <abhi.sharma10220@gmail.com>
2025-11-13 15:14:36 +0530

    added basic model

--- src/main/java/org/example/com/uberreviewservice/models/Review.java
+++ src/main/java/org/example/com/uberreviewservice/models/Review.java
@@ -1,6 +1,12 @@
 package org.example.com.uberreviewservice.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
 
+    @Id
+    Long id;
 }
